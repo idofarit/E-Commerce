52e044fb4229c050f05fb34b1e88cbe9fec34742
idofarit <idofarit@gmail.com>
2024-02-06 13:26:05 +0530

    ui fix

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -144,13 +144,13 @@ const StripeCheckout = () => {
 
 const Wrapper = styled.section`
   form {
-    width: 30vw;
+    width: 50vw;
     align-self: center;
     box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
       0px 2px 5px 0px rgba(50, 50, 93, 0.1),
       0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
     border-radius: 7px;
-    padding: 40px;
+    padding: 20px;
   }
   input {
     border-radius: 6px;
@@ -277,7 +277,11 @@ const Wrapper = styled.section`
   }
   @media only screen and (max-width: 600px) {
     form {
-      width: 80vw;
+      width: 95vw;
+      padding: 11px;
+    }
+    article {
+      text-align: center;
     }
   }
 `;
